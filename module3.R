d5e6b6241466add05b7d0a166dd2a80934b2a244
Joaquin-Monque <156555196+Joaquin-Monque@users.noreply.github.com>
2024-02-04 19:52:49 -0500

    Add files via upload

---
+++ module3.R
@@ -0,0 +1,9 @@
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+ABC_results <- c(4, 62, 51, 21, 2, 14, 15)
+CBS_results <- c(12, 75, 43, 19, 1, 21, 19)
+
+# Create a data frame with the data
+poll_matrix <- data.frame(Name, ABC_results, CBS_results)
+
+# Print the data frame
+print(poll_matrix)
