9a00156d4c5c9c9d8a07ec79c52201aecaee3760
Joaquin-Monque <156555196+Joaquin-Monque@users.noreply.github.com>
2024-02-25 20:00:38 -0500

    Add files via upload

---
+++ module5.R
@@ -0,0 +1,11 @@
+A <- matrix(1:100, nrow = 10)
+
+B <- matrix(1:1000, nrow = 10)
+
+inverse_A <- solve(A)
+inverse_B <- solve(B)
+
+determinant_A <- det(A)
+determinant_A
+determinant_B <- det(B)
+determinant_B
